cc6dd96e691d95dd82d51daa0607e63aa807d813
Paul Spence <paul.spence@amido.com>
2024-02-02 16:58:28 +0000

    nvim: adding neo tree to the lua config

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,7 +37,11 @@ local plugins = {
 	},
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = { 'nvim-lua/plenary.nvim' } },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-neo-tree/neo-tree.nvim", branch="v3.x", dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim"
+    } }
 }
 
 local opts = {}
